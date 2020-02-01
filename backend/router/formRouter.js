1fcc950122c020572e4b437877c01156456484dc
Szymon <szymon.ludyga2@gmail.com>
2020-02-01 20:06:44 +0100

    Adding eslint and first format of the existing code

--- backend/router/formRouter.js
+++ backend/router/formRouter.js
@@ -1,13 +1,16 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.post('',  (req, res) => {
-    const { email, firstName, lastName, eventDate } = req.body;
+router.post('', (req, res) => {
+    const {
+        email, firstName, lastName, eventDate,
+    } = req.body;
     console.log('Email:', email);
     console.log('First name:', firstName);
     console.log('Last name:', lastName);
     console.log('Event date:', eventDate);
-    res.status(200).send({ status: "OK" });
+    res.status(200).send({ status: 'OK' });
 });
 
 module.exports = router;
